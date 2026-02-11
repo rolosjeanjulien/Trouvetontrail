dde22ac71c5b8e04c667be4145bb5dfbd317235f
rolosjeanjulien <rolosjeanjulien@gmail.com>
2026-02-11 22:03:09 +0100

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_BACKEND_URL;
+const API_URL = process.env.REACT_APP_BACKEND_URL || 'https://trouvetontrail.onrender.com';
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
